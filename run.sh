bd6b2e8f5ef7edfe266493cd15036a926670215e
Magdalena Cullen <106096956+mcullen212@users.noreply.github.com>
2024-09-30 23:17:00 -0300

    Modified files to use gdb

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0
+if [[ "$1" = "gdb" ]]; then
+    qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512
+else
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+fi
 
 
